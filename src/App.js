0b254d075e231f0df72301e1084858a66119dc8a
Javaloq <jinomov573@gmail.com>
2024-12-07 15:21:17 +0500

    java

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {} from "react-router-dom"
+import React from 'react';
+import Home from './Home.js';
+
+const App = () => {
+    return(
+      <Home/>
+    )
+    
+
+};
+
+export default App;
